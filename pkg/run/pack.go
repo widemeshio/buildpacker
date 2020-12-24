a2e86d416f52e8f2be409ae7a027d2c3acd47a3e
bithavoc <im@bithavoc.io>
2020-12-24 17:10:52 +0000

    call pack

--- pkg/run/pack.go
+++ pkg/run/pack.go
@@ -1,5 +1,10 @@
 package run
 
+import (
+	"os"
+	"os/exec"
+)
+
 // Config runs the pack command
 type Config struct {
 	Path       string
@@ -16,5 +21,18 @@ type Pack struct {
 
 // Run runs pack command
 func (pack *Pack) Run() error {
+	args := []string{"build", "--path", pack.Path, "--trust-builder", "--builder", pack.Builder}
+	for _, bp := range pack.Buildpacks {
+		args = append(args, "--buildpack", bp)
+	}
+	args = append(args, pack.ImageTag)
+	cmd := exec.Command("pack", args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Env = os.Environ()
+	if err := cmd.Run(); err != nil {
+		return err
+	}
 	return nil
 }
